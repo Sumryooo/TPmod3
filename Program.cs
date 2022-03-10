b14da75b976c618e741bd00fed9906dbf9eaddad
sumryooo <adha.surya2002@gmail.com>
2022-03-10 23:17:21 +0700

    menambahkan implementasi dengan state-based construction

--- Program.cs
+++ Program.cs
@@ -32,6 +32,43 @@ namespace tpmod3_1302204026
 
             Console.WriteLine();
 
+
+
+            DoorMachine.Mulai();
+
+        }
+    }
+    class DoorMachine
+    {
+        enum State { terkunci, terbuka, exit };
+
+        public static void Mulai()
+        {
+            Console.WriteLine("Masukan State");
+            State state = State.terkunci;
+            while (state != State.exit)
+            {
+                string command = Console.ReadLine();
+                switch (command)
+                {
+                    case "terkunci":
+                        Console.WriteLine("Pintu terkunci");
+                        Console.WriteLine();
+                        break;
+                    case "terbuka":
+                        Console.WriteLine("Pintu tidak terkunci");
+                        Console.WriteLine();
+                        break;
+                    case "exit":
+                        state = State.exit;
+                        break;
+                    default:
+                        Console.WriteLine("Masukan ulang State");
+                        Console.WriteLine();
+                        break;
+                }
+            }
+            Console.WriteLine("Program Berhenti");
         }
     }
 }
